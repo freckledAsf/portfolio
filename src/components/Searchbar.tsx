abf281da684b5ae1efb3ea1d3e01934b343b8c81
Victor Tamayo Viera <106868505+freckledAsf@users.noreply.github.com>
2024-03-12 05:17:18 +0100

    feat: implement the right color theming in searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -12,19 +12,19 @@ const Searchbar = ({ onSearch, className }: Props) => {
   return (
     <div
       className={cn(
-        "flex gap-2 py-1 px-2 items-center bg-card border border-border rounded-full group",
+        "flex gap-2 py-1 px-2 items-center bg-card border border-input rounded-full group",
         className
       )}
     >
       <label htmlFor="search">
-        <MagnifyingGlassIcon className="w-4 h-4 text-input group-hover:text-card-foreground group-focus:text-card-foreground transition-colors" />
+        <MagnifyingGlassIcon className="w-4 h-4 text-muted-foreground group-hover:text-card-foreground group-focus-within:text-card-foreground transition-colors" />
       </label>
       <input
         id="search"
         type="text"
         onChange={(e) => onSearch(e.target.value)}
         placeholder="Search for a project..."
-        className="appearance-none bg-transparent w-full border-none focus:outline-none text-card-foreground placeholder-input"
+        className="appearance-none bg-transparent w-full border-none focus:outline-none text-card-foreground placeholder:text-muted-foreground"
       />
     </div>
   );
