61051a4f40617fa706870d4c337b5fc265fdb1f4
Victor Tamayo Viera <106868505+freckledAsf@users.noreply.github.com>
2024-03-12 04:10:12 +0100

    feat: add profile constant

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -1,43 +1,74 @@
-export const cards = [
+export const profile = {
+  title: "Victor Tamayo Viera",
+  description: "Estudiante Web Dev de 19 años.",
+  src: "/profile_picture.png",
+  alt: "Profile picture of Victor Tamayo Viera",
+  links: [
+    "https://github.com/freckledAsf",
+    "https://www.linkedin.com/in/victor-tamayo-viera/",
+    "https://discord.com/invite/R9QX5wrU3d",
+    "https://spark2ai.slack.com/team/U060211ABM3",
+    "mailto:vtviera67@gmail.com",
+  ],
+};
+
+export const projects = [
   {
-    title: "Victor Tamayo Viera",
-    description: "Estudiante Web Dev de 19 años.",
-    src: "/profile_picture.png",
-    alt: "Profile picture of Victor Tamayo Viera",
-    links: [
-      "https://github.com/freckledAsf",
-      "https://www.linkedin.com/in/victor-tamayo-viera/",
-      "https://discord.com/invite/R9QX5wrU3d",
-      "https://spark2ai.slack.com/team/U060211ABM3",
-      "mailto:vtviera67@gmail.com",
-    ],
-  },
-  {
+    id: "spark2",
     title: "Spark2",
     description: "The Faster way to find talent.",
     src: "/spark2.png",
     alt: "Spark2 logo",
+    fit: true,
+    color: {
+      background: "bg-[#8B5CF6]",
+      foreground: "text-[#FFFFFF]",
+    },
     links: ["https://spark2.ai/"],
   },
   {
+    id: "bictorchat",
     title: "BictorChat",
     description: "Chat with me!",
     src: "/bictorchat.svg",
     alt: "Spark2 logo",
-    links: ["https://github.com/freckledAsf/bictorchat", "https://bictorchat.vercel.app/"],
+    color: {
+      background: "bg-[#39E991]",
+      foreground: "text-[#262833]",
+    },
+    links: [
+      "https://github.com/freckledAsf/bictorchat",
+      "https://bictorchat.vercel.app/",
+    ]
   },
   {
+    id: "bictormp3",
     title: "BictorMP3",
     description: "Beats by Bictor.",
     src: "/bictormp3.svg",
     alt: "Spark2 logo",
-    links: ["https://github.com/freckledAsf/bictor.mp3", "https://demomp3.vercel.app/"],
+    color: {
+      background: "bg-gradient-to-r from-[#EF4236] to-[#FBB040]",
+      foreground: "text-[#111111]",
+    },
+    links: [
+      "https://github.com/freckledAsf/bictor.mp3",
+      "https://demomp3.vercel.app/",
+    ],
   },
   {
+    id: "registry",
     title: "Registry",
     description: "A better way to punch in at work.",
     src: "/registry.svg",
     alt: "Spark2 logo",
-    links: ["https://github.com/freckledAsf/dashboard", "https://pecoso.vercel.app/"],
+    color: {
+      background: "bg-[#000000]",
+      foreground: "text-[#F1F1F1]",
+    },
+    links: [
+      "https://github.com/freckledAsf/dashboard",
+      "https://pecoso.vercel.app/",
+    ],
   },
 ];
