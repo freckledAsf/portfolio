81b73a5cef4454ad00cf3f972ef7231054497499
Victor Tamayo Viera <106868505+freckledAsf@users.noreply.github.com>
2024-03-12 01:12:44 +0100

    feat: add card constants

---
+++ src/app/constants.tsx
@@ -0,0 +1,43 @@
+export const cards = [
+  {
+    title: "Victor Tamayo Viera",
+    description: "Estudiante Web Dev de 19 años.",
+    src: "/profile_picture.png",
+    alt: "Profile picture of Victor Tamayo Viera",
+    links: [
+      "https://github.com/freckledAsf",
+      "https://www.linkedin.com/in/victor-tamayo-viera/",
+      "https://discord.com/invite/R9QX5wrU3d",
+      "https://spark2ai.slack.com/team/U060211ABM3",
+      "mailto:vtviera67@gmail.com",
+    ],
+  },
+  {
+    title: "Spark2",
+    description: "The Faster way to find talent.",
+    src: "/spark2.png",
+    alt: "Spark2 logo",
+    links: ["https://spark2.ai/"],
+  },
+  {
+    title: "BictorChat",
+    description: "Chat with me!",
+    src: "/bictorchat.svg",
+    alt: "Spark2 logo",
+    links: ["https://github.com/freckledAsf/bictorchat", "https://bictorchat.vercel.app/"],
+  },
+  {
+    title: "BictorMP3",
+    description: "Beats by Bictor.",
+    src: "/bictormp3.svg",
+    alt: "Spark2 logo",
+    links: ["https://github.com/freckledAsf/bictor.mp3", "https://demomp3.vercel.app/"],
+  },
+  {
+    title: "Registry",
+    description: "A better way to punch in at work.",
+    src: "/registry.svg",
+    alt: "Spark2 logo",
+    links: ["https://github.com/freckledAsf/dashboard", "https://pecoso.vercel.app/"],
+  },
+];
